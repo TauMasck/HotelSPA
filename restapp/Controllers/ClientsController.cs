c107f85b1a3cc62498817e25a8b7e82bdf07072f
modliszka <nikapia@wp.pl>
2015-11-23 21:16:40 +0100

    Refactor by making repository for connectiong to data model

--- restapp/Controllers/ClientsController.cs
+++ restapp/Controllers/ClientsController.cs
@@ -1,4 +1,5 @@
 ﻿using RestApp.Models;
+using RestApp.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,54 +17,22 @@ namespace RestApp.Controllers
 {
     public class ClientsController : ApiController
     {
-        private HotelSPADataContext context;
+        private ClientRepository repository;
 
-        public ClientsController()
-        {
-            context = new HotelSPADataContext();
+        public ClientsController() {
+            this.repository = new ClientRepository();
         }
 
         // GET clients
         public IEnumerable<Client> Get()
         {
-            List<Client> Clients = new List<Client>();
-
-            var query = from client in context.Clients
-                        select client;
-            var clients = query.ToList();
-            foreach (var data in clients)
-            {
-                Clients.Add(new Client()
-                {
-                    Id = data.Id,
-                    NameSurname = data.Name_surname,
-                    IdNumber = data.Id_number,
-                    Company = data.Company,
-                    RoomNumber = data.Room_number,
-                    IsHere = data.Is_here == 0 ? false : true,
-                    Vegetarian = data.Vegetarian == 0 ? false : true,
-                    Questionnaire = data.Questionnaire == 0 ? false : true,
-                    Invoice = data.Invoice == 0 ? false : true
-                });
-            }
-            return Clients;
+            return repository.GetClients();
         }
 
         // GET clients/{id}
         public Client Get(int id)
         {
-            Client client = context.Clients.Where(x => x.Id == id)
-                .Select(x => new Client() {
-                    Id = x.Id,
-                    NameSurname = x.Name_surname,
-                    IdNumber = x.Id_number,
-                    Company = x.Company,
-                    RoomNumber = x.Room_number,
-                    IsHere = x.Is_here == 0 ? false : true,
-                    Vegetarian = x.Vegetarian == 0 ? false : true,
-                    Questionnaire = x.Questionnaire == 0 ? false : true,
-                    Invoice = x.Invoice == 0 ? false : true
-                }).SingleOrDefault();
+            Client client = repository.GetClientById(id);
  
             if (client == null)
             {
@@ -84,21 +53,7 @@ namespace RestApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                Clients client = new Clients()
-                {
-                    Id = model.Id,
-                    Name_surname = model.NameSurname,
-                    Id_number = model.IdNumber,
-                    Company = model.Company,
-                    Room_number = model.RoomNumber,
-                    Is_here = model.IsHere ? 0 : 1,
-                    Vegetarian = model.Vegetarian ? 0 : 1,
-                    Questionnaire = model.Questionnaire ? 0 : 1,
-                    Invoice = model.Invoice ? 0 : 1,
-                };
-
-                context.Clients.InsertOnSubmit(client);
-                context.SubmitChanges();
+                Clients client = repository.CreateClient(model);
 
                 var response = Request.CreateResponse(HttpStatusCode.Created, client);
 
@@ -122,16 +77,7 @@ namespace RestApp.Controllers
                 //ew.2 łapanie po różnych wyjątkach tak jak niżej jest
                 try
                 {
-                    var client = context.Clients.Single(x => x.Id == id);
-                    client.Name_surname = model.NameSurname;
-                    client.Id_number = model.IdNumber;
-                    client.Company = model.Company;
-                    client.Room_number = model.RoomNumber;
-                    client.Is_here = model.IsHere ? 0 : 1;
-                    client.Vegetarian = model.Vegetarian ? 0 : 1;
-                    client.Questionnaire = model.Questionnaire ? 0 : 1;
-                    client.Invoice = model.Invoice ? 0 : 1;
-                    context.SubmitChanges();
+                    repository.UpdateClientById(id, model);
                 }
 
                 catch
@@ -153,9 +99,7 @@ namespace RestApp.Controllers
             // tu tak samo
             try
             {
-                var client = context.Clients.Single(x => x.Id == id);
-                context.Clients.DeleteOnSubmit(client);
-                context.SubmitChanges();
+                var client = repository.DeleteClientById(id);
 
                 return Request.CreateResponse(HttpStatusCode.OK, client);
             }
