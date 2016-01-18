517f9c0a33b96cdc821738a3931924b825424053
modliszka <nikapia@wp.pl>
2016-01-18 20:18:18 +0100

    Add context to auth databases

--- restapp/Models/IdentityModels.cs
+++ restapp/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace RestApp.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("SQLConnection", throwIfV1Schema: false)
         {
         }
         
